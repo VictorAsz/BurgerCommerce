30335ff28c2046f1987e1c6dc88bf5bff5f15a3b
Victor Assunção <victor.a.schumann@gmail.com>
2024-01-24 16:13:07 -0300

    navbar created

---
+++ src/components/Navbar.vue
@@ -0,0 +1,23 @@
+<template>
+
+
+    <nav id="nav"> 
+        <router-link to="/">
+            <img src="" id="logo">
+        </router-link>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </nav>
+</template>
+
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
+
+
+<style scoped>
+
+
+</style>
