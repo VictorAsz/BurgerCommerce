dc7d38dcdf78d1f0d3223e320f539bb27274d361
Victor Assunção <victor.a.schumann@gmail.com>
2024-02-13 19:46:23 -0300

    burguerform component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,12 @@
 <script>
 
 import Banner from '@/components/Banner.vue';
-
+import BurguerForm from '@/components/BurguerForm.vue';
 export default{
   name: "Home",
   components: {
-    Banner
+    Banner,
+    BurguerForm
   }
 
 
@@ -18,7 +19,7 @@ export default{
   <div class="main-container">
     <Banner/>
      <h1>Monte o seu burger</h1>
-     <p>Formulario</p>
+     <BurguerForm/>
   </div>
 </template>
 
