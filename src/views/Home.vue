b88b8f2a530e8a37cd065f2dd2a2fa8478d4cdff
Victor Assunção <victor.a.schumann@gmail.com>
2024-02-13 19:27:49 -0300

    home

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,9 +1,24 @@
+<script>
+
+import Banner from '@/components/Banner.vue';
+
+export default{
+  name: "Home",
+  components: {
+    Banner
+  }
+
+
+}
+</script>
+
+
 <template>
+  <p>banner</p>
   <div class="main-container">
-     <h1>HOME</h1>
+    <Banner/>
+     <h1>Monte o seu burger</h1>
+     <p>Formulario</p>
   </div>
 </template>
 
-<script>
-
-</script>
