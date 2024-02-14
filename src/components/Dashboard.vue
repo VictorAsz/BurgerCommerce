0fc8796098d84e24d4c6b32afc81c3f3435ed99b
Victor Assunção <victor.a.schumann@gmail.com>
2024-02-14 15:50:27 -0300

    Script and style dashboard

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -1,7 +1,67 @@
 <script>
-export default {
+ export default {
+    name: "Dashboard",
+    data() {
+      return {
+        burgers: null,
+        burger_id: null,
+        status: []
+      }
+    },
+    methods: {
+      async getPedidos() {
+        const req = await fetch('http://localhost:3000/burgers')
+
+        const data = await req.json()
+
+        this.burgers = data
+
     
-}
+        this.getStatus()
+
+      },
+      async getStatus() {
+
+        const req = await fetch('http://localhost:3000/status')
+
+        const data = await req.json()
+
+        this.status = data
+
+      },
+      async deleteBurger(id) {
+
+        const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+          method: "DELETE"
+        });
+
+        const res = await req.json()
+
+        this.getPedidos()
+
+      },
+      async updateBurger(event, id) {
+
+        const option = event.target.value;
+
+        const dataJson = JSON.stringify({status: option});
+
+        const req = await fetch(`http://localhost:3000/burgers/${id}`, {
+          method: "PATCH",
+          headers: { "Content-Type" : "application/json" },
+          body: dataJson
+        });
+
+        const res = await req.json()
+
+        console.log(res)
+
+      }
+    },
+    mounted () {
+    this.getPedidos()
+    }
+  }
 </script>
 <template>
     <div id="burger-table" v-if="burgers">
@@ -43,5 +103,59 @@ export default {
 </template>
 
 <style scoped>
+ #burger-table {
+    max-width: 1200px;
+    margin: 0 auto;
+  }
 
+  #burger-table-heading,
+  #burger-table-rows,
+  .burger-table-row {
+    display: flex;
+    flex-wrap: wrap;
+  }
+
+  #burger-table-heading {
+    font-weight: bold;
+    padding: 12px;
+    border-bottom: 3px solid #333;
+  }
+
+  .burger-table-row {
+    width: 100%;
+    padding: 12px;
+    border-bottom: 1px solid #CCC;
+  }
+
+  #burger-table-heading div,
+  .burger-table-row div {
+    width: 19%;
+  }
+
+  #burger-table-heading .order-id,
+  .burger-table-row .order-number {
+    width: 5%;
+  }
+
+  select {
+    padding: 12px 6px;
+    margin-right: 12px;
+  }
+
+  .delete-btn {
+    background-color: #222;
+    color:#fcba03;
+    font-weight: bold;
+    border: 2px solid #222;
+    padding: 10px;
+    font-size: 16px;
+    margin: 0 auto;
+    cursor: pointer;
+    transition: .5s;
+  }
+  
+  .delete-btn:hover {
+    background-color: transparent;
+    color: #222;
+  }
 </style>
